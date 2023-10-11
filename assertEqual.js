2dfb6d7873e7f4c2104608e7ee4288b4f96d0af7
kvirani-test <khurram@lighthouselabs.ca>
2023-10-11 17:49:52 +0000

    bleh3

--- assertEqual.js
+++ assertEqual.js
@@ -1,1 +1,7 @@
-console.log('hello!');
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    }
+  };
